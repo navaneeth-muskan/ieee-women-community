d85acae252cbfa0da2af21fd6f7868972d303394
mac developer <macdeveloper999@gmail.com>
2026-02-16 06:14:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8177219355-def1f",
+  "appId": "1:15125208128:web:ff34c43f40fdf2a6b2d3a5",
+  "apiKey": "AIzaSyDk-c1XxM_wTEw8Ow-otN7zckBxp-rWRU0",
+  "authDomain": "studio-8177219355-def1f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "15125208128"
+};
