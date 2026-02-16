0d92766451e8fdcf5414b2935da21f2d1e3e50f5
mac developer <macdeveloper999@gmail.com>
2026-02-16 06:35:16 +0000

    manually i add it from the project you created..what all things we need

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-8177219355-def1f",
-  "appId": "1:15125208128:web:ff34c43f40fdf2a6b2d3a5",
-  "apiKey": "AIzaSyDk-c1XxM_wTEw8Ow-otN7zckBxp-rWRU0",
-  "authDomain": "studio-8177219355-def1f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "15125208128"
+  apiKey: "AIzaSyDk-c1XxM_wTEw8Ow-otN7zckBxp-rWRU0",
+  authDomain: "studio-8177219355-def1f.firebaseapp.com",
+  projectId: "studio-8177219355-def1f",
+  storageBucket: "studio-8177219355-def1f.firebasestorage.app",
+  messagingSenderId: "15125208128",
+  appId: "1:15125208128:web:ff34c43f40fdf2a6b2d3a5",
+  measurementId: ""
 };
